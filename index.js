35302a051f896e9016cb2fa479f94acd2c96b677
MattIPv4 <me@mattcowley.co.uk>
2021-01-30 15:53:20 +0000

    Add handling for maps/sets

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const v8 = require('v8');
 
-// Thanks to https://github.com/miktam/sizeof for the basis of this
+// Thanks to https://github.com/miktam/sizeof for the basis of a lot of this
 
 const getSize = (object, seen, id) => {
     const objectType = typeof object;
@@ -11,15 +11,6 @@ const getSize = (object, seen, id) => {
             id,
             type: objectType,
             size: v8.serialize(object).byteLength,
-        };
-    }
-
-    // Null
-    if (object === null) {
-        return {
-            id,
-            type: 'null',
-            size: 0,
         };
     }
 
@@ -34,28 +25,17 @@ const getSize = (object, seen, id) => {
         };
     }
 
-    // Buffer
-    if (Buffer.isBuffer(object)) {
-        return {
-            id,
-            type: 'buffer',
-            size: object.byteLength,
-        };
-    }
-
-    // Array
-    if (Array.isArray(object)) {
-        const res = object.map((innerObject, i) => getSize(innerObject, seen, `${id}[${i}]`));
-        return {
-            id,
-            type: 'array',
-            size: res.reduce((acc, item) => acc + item.size, 0),
-            values: res,
-        };
-    }
-
     // Object
     if (objectType === 'object') {
+        // Null
+        if (object === null) {
+            return {
+                id,
+                type: 'null',
+                size: 0,
+            };
+        }
+
         // Handle circular objects
         const circularId = seen.get(object);
         if (circularId !== undefined) {
@@ -67,6 +47,58 @@ const getSize = (object, seen, id) => {
             };
         }
         seen.set(object, id);
+
+        // Buffer
+        if (Buffer.isBuffer(object)) {
+            return {
+                id,
+                type: 'buffer',
+                size: object.byteLength,
+            };
+        }
+
+        // Array
+        if (Array.isArray(object)) {
+            const res = object.map((innerObject, i) => getSize(innerObject, seen, `${id}[${i}]`));
+            return {
+                id,
+                type: 'array',
+                size: res.reduce((acc, item) => acc + item.size, 0),
+                values: res,
+            };
+        }
+
+        // Map & WeakMap
+        if (object instanceof Map || object instanceof WeakMap) {
+            const res = [...object.keys()].reduce((all, key) => {
+                const keyId = `${id}.${key.toString()}`;
+                const keySize = getSize(key, seen, `${keyId}\{key\}`);
+                const valueSize = getSize(object.get(key), seen, keyId);
+                all[key] = {
+                    key: keySize,
+                    value: valueSize,
+                    size: keySize.size + valueSize.size,
+                };
+                return all;
+            }, {});
+            return {
+                id,
+                type: 'map',
+                size: Object.values(res).reduce((acc, item) => acc + item.size, 0),
+                values: res,
+            };
+        }
+
+        // Set & WeakSet
+        if (object instanceof Set || object instanceof WeakSet) {
+            const res = [...object.values()].map((value, i) => getSize(value, seen, `${id}[${i}]`));
+            return {
+                id,
+                type: 'set',
+                size: Object.values(res).reduce((acc, item) => acc + item.size, 0),
+                values: res,
+            };
+        }
 
         // Get props
         const props = [];
